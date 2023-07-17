1322bbff9e92699fe6d37e9fc11c8882e1c11608
healim01 <22000770@handong.edu>
2023-07-17 22:30:10 +0900

    fix: change width

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,8 +5,6 @@ import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import FundingCard from '../Components/FundingCard';
 import Header from '../Components/Header';
-// import CreateButton from '../components/CreateButton';
-// import Footer from '../components/Footer';
 
 const funds = [
   {
@@ -75,36 +73,18 @@ const funds = [
   },
 ];
 
-const Host = styled.div`
-  background-color: black;
-  height: 250px;
+const Padding = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const HostImg = styled.img`
-  width: 100%;
-  height: 250px;
-  object-fit: cover;
-  background-size: cover;
-  position: relative;
-  opacity: 0.5;
+const Width = styled.div`
+  padding: 50px 0px;
+  width: 1000px;
 `;
 
-const HostDetail = styled.div`
-  position: absolute;
-  top: 30%;
-  padding: 0px 200px;
-  font-size: 30px;
-  color: white;
-`;
-
-const HostName = styled(HostDetail)`
-  top: 25%;
-  font-weight: bold;
-`;
-
-const PageContainer = styled.div`
-  padding: 50px 200px;
-`;
+const PageContainer = styled.div``;
 
 const Title = styled.div`
   display: flex;
@@ -124,14 +104,12 @@ const Funds = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
-  min-width: 980px;
-  padding: 0px 200px;
 `;
 
 const Grids = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 10px;
+  gap: 20px;
 `;
 
 function Home() {
@@ -150,19 +128,23 @@ function Home() {
   return (
     <>
       <Header />
-      <PageContainer>
-        <Title> 현재 진행 중인 펀딩 </Title>
-        <Detail>
-          <Num>{funds.length}</Num> 개의 프로젝트가 있습니다.
-        </Detail>
-      </PageContainer>
-      <Funds>
-        <Grids>
-          {funds.map((fund) => (
-            <FundingCard key={fund.funding_id} fund={fund} />
-          ))}
-        </Grids>
-      </Funds>
+      <Padding>
+        <Width>
+          <PageContainer>
+            <Title> 현재 진행 중인 펀딩 </Title>
+            <Detail>
+              <Num>{funds.length}</Num> 개의 프로젝트가 있습니다.
+            </Detail>
+          </PageContainer>
+          <Funds>
+            <Grids>
+              {funds.map((fund) => (
+                <FundingCard key={fund.funding_id} fund={fund} />
+              ))}
+            </Grids>
+          </Funds>
+        </Width>
+      </Padding>
       {/* <CreateButton roomId={roomId} room_name={room?.room_name} /> */}
       {/* <Footer /> */}
     </>
